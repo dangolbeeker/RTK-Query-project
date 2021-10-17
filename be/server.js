4b48df7e121e713f1fb33dd5bdec123e84addfb1
dangolbeeker <scottb305@outlook.com>
2021-10-17 02:58:51 -0500

    backend overhaul work in progress left out postgraphile

---
+++ be/server.js
@@ -0,0 +1,110 @@
+const fastify = require('fastify')()
+const mq = require('mqemitter')
+const mercurius = require('..')
+
+async function start () {
+    const app = Fastify()
+
+    fastify.register(require('fastify-postgres'), {
+        connectionString: 'postgres://postgres@localhost/postgres',
+        native: true
+      })
+      
+      fastify.get('/user/:id', (req, reply) => {
+        fastify.pg.query(
+          'SELECT id, username, hash, salt FROM users WHERE id=$1', [req.params.id],
+          function onResult (err, result) {
+            reply.send(err || result)
+          }
+        )
+      })
+      
+    const schema = `
+    type Like {
+        id: Boolean!
+    } 
+    type Notification {
+        id: ID!
+        message: String
+      }
+      type Mutation {
+        addNotification(message: String): Notification
+      }
+      type Subscription {
+        notificationAdded: Notification
+      }
+    `
+    const emitter = mq()
+
+    const resolvers = {
+        Mutation: {
+          addNotification: async (_, { message }) => {
+            const notification = {
+              id: 1,
+              message
+            }
+            await emitter.emit({
+              topic: 'NOTIFICATION_ADDED',
+              payload: {
+                notificationAdded: notification
+              }
+            })
+    
+            return notification
+          }
+        },
+        Subscription: {
+          notificationAdded: {
+            subscribe: (root, args, { pubsub }) => pubsub.subscribe('NOTIFICATION_ADDED')
+          }
+        }
+      }
+    
+      app.register(mercurius, {
+        schema,
+        resolvers,
+        subscription: {
+          emitter
+        }
+      })
+    
+      await app.ready()
+    
+      app.graphql.addHook('preSubscriptionParsing', async function (schema, source, context) {
+        console.log('preSubscriptionParsing called')
+      })
+    
+      app.graphql.addHook('preSubscriptionExecution', async function (schema, document, context) {
+        console.log('preSubscriptionExecution called')
+      })
+    
+      app.graphql.addHook('onSubscriptionResolution', async function (execution, context) {
+        console.log('onSubscriptionResolution called')
+      })
+    
+      app.graphql.addHook('onSubscriptionEnd', async function (context) {
+        console.log('onSubscriptionEnd called')
+      })
+    
+     
+    
+}
+
+fastify.register(require('fastify-postgres'), {
+  connectionString: 'postgres://postgres@localhost/postgres',
+  native: true
+})
+
+fastify.get('/user/:id', (req, reply) => {
+  fastify.pg.query(
+    'SELECT id, username, hash, salt FROM users WHERE id=$1', [req.params.id],
+    function onResult (err, result) {
+      reply.send(err || result)
+    }
+  )
+})
+
+fastify.listen(3000, err => {
+  if (err) throw err
+  console.log(`server listening on ${fastify.server.address().port}`)
+})
